6466678938b0b26b447742ce66487cd7dfd92b56
Oldiex <93683481+Oldiex@users.noreply.github.com>
2023-04-13 20:58:57 +0300

    map was add

--- App.js
+++ App.js
@@ -1,20 +1,153 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,Alert, Image, SafeAreaView } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {useState, useEffect} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	// const [mapCords, setMapCords] = useState({
+	// 	latitude: 0,
+	// 	longitude: 0,
+	// 	latitudeDelta: 0,
+	// 	longitudeDelta: 0,
+	// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	// const [dragCaods, setDragCords] = useState({
+	// 	latitude: 0,
+	// 	longitude: 0
+	// })
+	useEffect(() => {
+		console.log(1)
+	}, [])
+
+	return (
+		<SafeAreaView style={{flex: 1}}>
+			<View style={styles.container}>
+				<MapView
+					style={styles.mapStyle}
+					initialRegion={{
+						latitude: 50.01212,
+						longitude: 36.22669,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+						// 50.01212121216565, 36.22669960780935
+				}}
+					customMapStyle={mapStyle}>
+					<Marker
+						draggable
+						coordinate={{
+							latitude: 49.97430,
+							longitude: 36.25886,
+						}}
+						onDragEnd={(e) => {
+							alert(JSON.stringify(e.nativeEvent.coordinate));
+							console.log(1);
+						}}
+						title={'Test Marker'}
+						description={'This is a description of the marker'}
+					/>
+				</MapView>
+			</View>
+		</SafeAreaView>
+	);
+};
+export default App;
+
+
+const mapStyle = [
+	{elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+	{elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+	{elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+	{
+		featureType: 'administrative.locality',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#d59563'}],
+	},
+	{
+		featureType: 'poi',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#d59563'}],
+	},
+	{
+		featureType: 'poi.park',
+		elementType: 'geometry',
+		stylers: [{color: '#263c3f'}],
+	},
+	{
+		featureType: 'poi.park',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#6b9a76'}],
+	},
+	{
+		featureType: 'road',
+		elementType: 'geometry',
+		stylers: [{color: '#38414e'}],
+	},
+	{
+		featureType: 'road',
+		elementType: 'geometry.stroke',
+		stylers: [{color: '#212a37'}],
+	},
+	{
+		featureType: 'road',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#9ca5b3'}],
+	},
+	{
+		featureType: 'road.highway',
+		elementType: 'geometry',
+		stylers: [{color: '#746855'}],
+	},
+	{
+		featureType: 'road.highway',
+		elementType: 'geometry.stroke',
+		stylers: [{color: '#1f2835'}],
+	},
+	{
+		featureType: 'road.highway',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#f3d19c'}],
+	},
+	{
+		featureType: 'transit',
+		elementType: 'geometry',
+		stylers: [{color: '#2f3948'}],
+	},
+	{
+		featureType: 'transit.station',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#d59563'}],
+	},
+	{
+		featureType: 'water',
+		elementType: 'geometry',
+		stylers: [{color: '#17263c'}],
+	},
+	{
+		featureType: 'water',
+		elementType: 'labels.text.fill',
+		stylers: [{color: '#515c6d'}],
+	},
+	{
+		featureType: 'water',
+		elementType: 'labels.text.stroke',
+		stylers: [{color: '#17263c'}],
+	},
+	];
+	const styles = StyleSheet.create({
+	container: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+	},
+	mapStyle: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
 });
